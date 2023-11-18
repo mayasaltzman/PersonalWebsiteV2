f88beb7c5824f8bdb56c9eaab0afe6197e94ae22
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-18 13:40:40 -0500

    set up routes and router

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import { Route, Routes} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
+        <NavBar />
+        <Routes>
+          {/* adding pages in these route tags, curly braces are the components which are the js files */}
+          <Route path="/" element={<home />} />
+          <Route path="/experience" element={<experience />} />
+          <Route path="/projects" element={<projects />} />
+          <Route path="/resume" element={<resume />} />
+        </Routes>
 
-      </header>
     </div>
   );
 }
