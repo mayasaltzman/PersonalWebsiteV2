9bccea4533e96b2c4caf619185baaf91b6a47aef
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-18 16:14:03 -0500

    added github links

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -5,11 +5,6 @@ import Experience from './components/pages/experience';
 import Resume from './components/pages/resume';
 import Projects from './components/pages/projects';
 import Kenna from './components/pages/kenna';
-import Intro from './components/pages/kenna#intro'
-import About from './components/pages/kenna#about'
-import Description from './components/pages/kenna#description'
-import Goals from './components/pages/kenna#goals'
-import CA from './components/pages/kenna#ca'
 
 
 function App() {
@@ -23,11 +18,6 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/kenna" element={<Kenna />} />
-        <Route path="/kenna#intro" element={<Intro />} />
-        <Route path="/kenna#description" element={<Description />} />
-        <Route path="/kenna#goals" element={<Goals />} />
-        <Route path="/kenna#about" element={<About />} />
-        <Route path="/kenna#ca" element={<CA />} />
        </Routes>
       <kenna/>
 
--- my-site/src/components/pages/projects.js
+++ my-site/src/components/pages/projects.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const projects = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <h1>Featured Projects</h1>
+            <Link to="https://github.com/mayasaltzman/Molecule-Manipulation-Library"><button>Molecule Manipulation Library</button></Link>
+            <Link to="https://github.com/mayasaltzman/TicTacToe-Game-Suite"><button>TicTacToe Game Suite</button></Link>
+            <Link to="https://github.com/mayasaltzman/habit_tracker"><button>Habit Tracker</button></Link>
+        </div>
+    )
 }
 
 export default projects
