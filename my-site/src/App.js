40eb0dd0fd672ad6b45801af2f4b799dbd54c953
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-18 13:51:36 -0500

    added basic styles to nav bar so I know whats up

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import { Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
+import Experience from './components/pages/experience';
+import Resume from './components/pages/resume';
+import Projects from './components/pages/projects';
+
 
 function App() {
   return (
     <div className="App">
-        <NavBar />
-        <Routes>
-          {/* adding pages in these route tags, curly braces are the components which are the js files */}
-          <Route path="/" element={<home />} />
-          <Route path="/experience" element={<experience />} />
-          <Route path="/projects" element={<projects />} />
-          <Route path="/resume" element={<resume />} />
-        </Routes>
+      <NavBar />
+      <Routes>
+        {/* adding pages in these route tags, curly braces are the components which are the js files */}
+        <Route path="/" element={<home />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
 
     </div>
   );
