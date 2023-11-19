ac702fde8040526bec9515f69466dafaeea7b25c
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-19 14:29:07 -0500

    added icons and styles to featured projects

--- my-site/src/components/pages/projects.js
+++ my-site/src/components/pages/projects.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import "./projects.css"
 
 const projects = () => {
     return (
         <div>
             <h1>Featured Projects</h1>
-            <Link to="https://github.com/mayasaltzman/Molecule-Manipulation-Library"><button>Molecule Manipulation Library</button></Link>
-            <Link to="https://github.com/mayasaltzman/TicTacToe-Game-Suite"><button>TicTacToe Game Suite</button></Link>
-            <Link to="https://github.com/mayasaltzman/habit_tracker"><button>Habit Tracker</button></Link>
+            <div id='buttons'>
+                <Link to="https://github.com/mayasaltzman/Molecule-Manipulation-Library" class="project-link"><button class="project"><img src="molecule.png" alt="Black and white cartoon molecule" id='molecule' /><h3>Molecule Manipulation Library</h3>Full stack web app for viewing molecules</button></Link>
+                <Link to="https://github.com/mayasaltzman/TicTacToe-Game-Suite" class="project-link"><button class="project"><img src="tictactoe.png" alt="Black and white cartoon tic tac toe game" id='molecule'/><h3>TicTacToe Game Suite</h3>GUI with Tic Tac Toe, Numerical Tic Tac Toe, and Player Profiles</button></Link>
+                <Link to="https://github.com/mayasaltzman/habit_tracker" class="project-link"><button class="project"> <img src="tracker.png" id="molecule" alt="cartoon black and white calendar"/><h3>Habit Tracker</h3>Full stack web app to keep on track with your habits (work in progress)</button></Link>
+            </div>
+
         </div>
     )
 }
