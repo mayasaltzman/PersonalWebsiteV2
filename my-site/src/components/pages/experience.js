c3ea0c871e9f43a4bb8cf90d86857658174c1ab3
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-19 13:46:34 -0500

    added style sheets

--- my-site/src/components/pages/experience.js
+++ my-site/src/components/pages/experience.js
@@ -4,8 +4,11 @@ import "./experience.css"
 
 const experience = () => {
   return (
-    <div id='buttons'>
-      <Link to= "/kenna" class ="link"><button class='button'><img src="kenna_logo.jpg" alt="Kenna logo" id='kenna-logo'/> <h3>Kenna</h3>Customer First Thinking</button></Link>
+    <div>
+      <h1>Work Experience</h1>
+      <div id='buttons'>
+        <Link to="/kenna" class="link"><button class='button'><img src="kenna_logo.jpg" alt="Kenna logo" id='kenna-logo' /> <h3>Kenna</h3>Customer First Thinking</button></Link>
+      </div>
     </div>
   )
 }
