454fb680585f6d0c30b951c386f06793532a070a
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-19 15:58:31 -0500

    added some color to home page

--- my-site/src/components/pages/home.js
+++ my-site/src/components/pages/home.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import "./home.css"
 
 const home = () => {
   return (
     <div>
-      <h1>Hello World</h1>
-      <img src='profilephoto.png' alt='maya' />
-      <p>I am currently a third year Computer Science Honors Co-op student at the University of Guelph. I most recently spent an eight month co-op work term at Kenna in the Data Management Department. I am passionate about environmentally sustainable tech. I enjoy learning about ways developers can combine tech and ethics!</p>
-      <p>I am the President of the Guelph Coding Community at the University of Guelph and love my campus computing community.</p>
-      <p>I like to spend my free time in nature or reading a book. I love camping and going on hikes.  I have experience leading canoe trips and have spent 90 days in the woods.
-        I have recently been enjoying trying out new vegan recipes.
-      </p>
-      <p>I’m inspired by people who want to make the tech industry more inclusive creatives. If that sounds like you, let's grab some coffee!</p>
+      <div id='header'>
+        <span id='wave'>&#128075;</span>
+        <h1>Hello World!</h1>
+      </div>
+      <div id='body'>
+        <p>I am currently a third year <span id='cs'>Computer Science Honors Co-op</span> student at the <span id='uni'>University of Guelph.</span> I most recently spent an eight month co-op work term at <span id='kenna'>Kenna</span> in the Data Management Department. I am passionate about environmentally sustainable tech. I enjoy learning about combining tech and ethics! <br /><br />
+          I am the <span id='pres'>President</span> of the <span id='GCC'>Guelph Coding Community </span>and love my computing community.
+          <br /><br />I like to spend my free time in nature or reading. You can find me camping and going on hikes. Over the years I have spent a total of 90 days in the woods.
+          I have recently been enjoying trying out new vegan recipes. <br /><br />I’m inspired by people who want to make the tech industry more inclusive! If this sounds like you, lets grab a coffee!</p>
+        <img id="pfp" src='profilephoto.png' alt='maya' />
+      </div>
+
     </div>
   )
 }
