cf95c42b19423ad72a27e3705ba22f89ba8ba87e
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-24 10:33:31 -0500

    added some styles to footer but its weirdly cut off

--- my-site/src/components/Contact.js
+++ my-site/src/components/Contact.js
@@ -4,7 +4,7 @@ import "./Contact.css"
 const projects = () => {
     return (
         <div id='contact'>
-            <footer>
+            <footer id='footer'>
                 <p>If you want to chat, email me at mayasaltzman3@gmail.com. </p>
                 <a href="https://github.com/mayasaltzman"><i id="socials" class="fa fa-github"></i></a>
                 <a href="https://www.linkedin.com/in/maya-saltzman/"><i id="socials" class="fa fa-linkedin"></i></a>
