506b5fd723634c18b5baae687dfd1c5a88b7a0da
Maya Saltzman <mayasaltzman3@gmail.com>
2023-11-28 11:45:52 -0500

    made changes to a few pages to look better on smaller screen sizes

--- my-site/src/components/pages/projects.js
+++ my-site/src/components/pages/projects.js
@@ -5,7 +5,7 @@ import "./projects.css"
 const projects = () => {
     return (
         <div>
-            <h1>Featured Projects</h1>
+            <h1 id='featured'>Featured Projects</h1>
             <div id='buttons'>
                 <Link to="https://github.com/mayasaltzman/Molecule-Manipulation-Library" class="project-link"><button class="project" id='p1'><img src="molecule.png" alt="Black and white cartoon molecule" id='molecule' /><h3>Molecule Manipulation Library</h3>Full stack web app for viewing molecules</button></Link>
                 <Link to="https://github.com/mayasaltzman/TicTacToe-Game-Suite" class="project-link"><button class="project" id='p2'><img src="tictactoe.png" alt="Black and white cartoon tic tac toe game" id='molecule'/><h3>TicTacToe Game Suite</h3>GUI with Tic Tac Toe, Numerical Tic Tac Toe, and Player Profiles</button></Link>
