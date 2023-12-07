971e84ca719c497e8219eeb36cc048bb55e4d179
Maya Saltzman <mayasaltzman3@gmail.com>
2023-12-07 11:45:41 -0500

    i messed up

--- my-site/src/index.js
+++ my-site/src/index.js
@@ -1,14 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
-
+function index() {
+    return (
+      <div className="index">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+  
+  export default index;
